9f69ad5cf6dad752d58f37a2111a9cb522fc0c01
Ariel Taitz <taitza@amazon.com>
2020-08-04 17:25:02 +0300

    PascalCase to camel_case

--- text/pascal_to_snake.py
+++ text/pascal_to_snake.py
@@ -6,6 +6,8 @@ pascal = sys.argv[1]
 with_spaces = []
 for c in pascal:
     if c.isupper():
-        with_spaces.append(' ')
-    with_spaces.append(c)
+        with_spaces.append('_')
+        with_spaces.append(c.lower())
+    else:
+        with_spaces.append(c)
 print(''.join(with_spaces[1:]))
