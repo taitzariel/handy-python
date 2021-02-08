bb638515fc8151fdde21b7982e7636473da4aa5f
Ariel Taitz <taitza@amazon.com>
2021-02-08 11:14:40 +0200

    from stdin instead of argv

--- text/pascal_insert_spaces.py
+++ text/pascal_insert_spaces.py
@@ -2,10 +2,10 @@
 
 import sys
 
-pascal = sys.argv[1]
-with_spaces = []
-for c in pascal:
-    if c.isupper():
-        with_spaces.append(' ')
-    with_spaces.append(c)
-print(''.join(with_spaces[1:]))
+for pascal in sys.stdin:
+    with_spaces = []
+    for c in pascal.rstrip():
+        if c.isupper():
+            with_spaces.append(' ')
+        with_spaces.append(c)
+    print(''.join(with_spaces[1:]))
