04121e416be66e0687ccbcd09d9a20daf6255fa5
Ariel Taitz <taitza@amazon.com>
2022-10-18 17:12:33 +0300

    convert python3 signature to python2 type comment

---
+++ python2/typing_to_python2.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+# simple - will not work for more complicated signatures
+
+import sys
+
+line = sys.stdin.read()
+
+if 'def ' in line:
+    line2, out_type = line.rsplit('->')
+    leading_space, line3 = line2.split('def ')
+    method_name, args = line3.split('(')
+    arg_names = []
+    arg_types = []
+    for arg in args.split(')')[0].split(','):
+        if ':' in arg:
+            arg_name, arg_type = arg.split(':')
+            arg_names.append(arg_name)
+            arg_types.append(arg_type)
+        else:
+            arg_names.append(arg)
+    arg_names_str = ', '.join(arg_names)
+    arg_types_str = ', '.join(arg_types)
+    print(f'{leading_space}def {method_name}({arg_names_str}):')
+    print(f'{leading_space}    # type: ({arg_types_str}) -> {out_type.split(":")[0]}')
+else:
+    print(line)
+
