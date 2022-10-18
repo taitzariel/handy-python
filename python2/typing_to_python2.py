e1c6fbeb511d37907e4245cd6040715473e4ba8d
Ariel Taitz <taitza@amazon.com>
2022-10-18 18:24:55 +0300

    default value

--- python2/typing_to_python2.py
+++ python2/typing_to_python2.py
@@ -12,10 +12,14 @@ if 'def ' in line:
     method_name, args = line3.split('(')
     arg_names = []
     arg_types = []
-    for arg in args.split(')')[0].split(','):
+    for arg in args.split(')')[0].split(', '):
         if ': ' in arg:
             arg_name, arg_type = arg.split(': ')
-            arg_names.append(arg_name)
+            if ' = ' in arg_type:
+                arg_type, default = arg_type.split(' = ')
+                arg_names.append(f'{arg_name}={default}')
+            else:
+                arg_names.append(arg_name)
             arg_types.append(arg_type)
         else:
             arg_names.append(arg)
